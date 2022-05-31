86f16151a67b3ce2eb62a62618201977469dcdaf
sriram <97436480+ram1494@users.noreply.github.com>
2022-05-31 19:54:36 +0530

    def pal(n):     rev=0     while n!=0:         i=n%10         rev=rev*10+i         n=n//10     return rev n=int(input()) a=list(map(int,input().split())) c=0 for i in range(n):     if pal(a[i])==a[i]:         c+=1 print(c)

---
+++ Count_palindromes.py
@@ -0,0 +1,14 @@
+def pal(n):
+    rev=0
+    while n!=0:
+        i=n%10
+        rev=rev*10+i
+        n=n//10
+    return rev
+n=int(input())
+a=list(map(int,input().split()))
+c=0
+for i in range(n):
+    if pal(a[i])==a[i]:
+        c+=1
+print(c)
