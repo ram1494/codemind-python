d29850aa43b1d4df3cab45d03575d8fe1d46ee3e
sriram <97436480+ram1494@users.noreply.github.com>
2022-07-03 19:51:04 +0530

    x=list(map(str,input().split())) x.reverse() for i in x:     print(i,end=' ')

--- Reverse_String.py
+++ Reverse_String.py
@@ -1,9 +1,4 @@
-def reverse(x):
-    revstring=""
-    for i in x:
-        revstring = i + revstring
-    print(revstring)
-    
-    
-x = str(input())
-reverse(x)
+x=list(map(str,input().split()))
+x.reverse()
+for i in x:
+    print(i,end=' ')
