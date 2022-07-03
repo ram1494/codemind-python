09a65260b738099208c3a5350e815bb6dc41ba6e
sriram <97436480+ram1494@users.noreply.github.com>
2022-07-03 17:43:25 +0530

    def happy_num(n):     r=s=0     while(n>0):         r=n%10         s+=r**2         n//=10     return s n=int(input()) res=n while(res!=1 and res!=4):     res=happy_num(res) if(res==1):     print("True") elif(res==4):     print("False")

---
+++ happy_number.py
@@ -0,0 +1,15 @@
+def happy_num(n):
+    r=s=0
+    while(n>0):
+        r=n%10
+        s+=r**2
+        n//=10
+    return s
+n=int(input())
+res=n
+while(res!=1 and res!=4):
+    res=happy_num(res)
+if(res==1):
+    print("True")
+elif(res==4):
+    print("False")
