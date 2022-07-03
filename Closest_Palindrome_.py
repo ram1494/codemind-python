5a97331bef498b514c356b00d0b0fac207626b2b
sriram <97436480+ram1494@users.noreply.github.com>
2022-07-03 17:37:33 +0530

    def palin(j):     m=j     s=0     while m!=0:         v=m%10         s=s*10+v         m=m//10     if j==s:         return j x=int(input()) for i in range(x-1,1,-1):     if(palin(i)):         a=i         break g=x+1 while g!=0:     if palin(g):         b=g         break     g+=1 if(x-a)<(b-x):     print(a) elif(x-a)==(b-x):     print(a,b) else:     print(b)

---
+++ Closest_Palindrome_.py
@@ -0,0 +1,26 @@
+def palin(j):
+    m=j
+    s=0
+    while m!=0:
+        v=m%10
+        s=s*10+v
+        m=m//10
+    if j==s:
+        return j
+x=int(input())
+for i in range(x-1,1,-1):
+    if(palin(i)):
+        a=i
+        break
+g=x+1
+while g!=0:
+    if palin(g):
+        b=g
+        break
+    g+=1
+if(x-a)<(b-x):
+    print(a)
+elif(x-a)==(b-x):
+    print(a,b)
+else:
+    print(b)
